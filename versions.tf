7e9cc7d00b9e7fe68214a941f370117999909527
Matthias Nolden <matthias.nolden@orange.com>
2022-03-31 16:02:35 +0200

    Update TF + AVTX Module Versioning

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 2.21.1"
+      version = "2.21.1-6.6.ga"
     }
     aws = {
       source  = "hashicorp/aws"
@@ -13,5 +13,5 @@ terraform {
       version = "~> 2.0.0"
     }
   }
-  required_version = ">= 1.1"
+  required_version = ">= 1.0"
 }
